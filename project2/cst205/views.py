33758a85716197425a58139ec4e8de5b4304da0a
leoneljunior <LeonelJR@MacBook-Pro-de-Raimundo.local>
2015-10-25 13:13:52 -0700

    Last Version

--- project2/cst205/views.py
+++ project2/cst205/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect, render_to_response
 import base64
 from PIL import Image
 from PIL import ImageFilter
+from PIL import ImageColor
+from PIL import ImageEnhance
 import json
 
 # Create your views here.
@@ -13,19 +15,75 @@ def home(request):
 def uploadImagePage(request):
     return render(request, 'uploadImage.html')
 
-def applyContour(request):
+def setEffect(request):
     
-    result = {"Success": True}
+    result = {}
+    
+    #Open Image to apply effects
+    im = Image.open("static/img/img.png")
+    image = im
+    filterInput = request.POST["effect"]
+    borderColor = request.POST["border"];
+    print filterInput + " " + borderColor
+    
+    #background.save('out.png')
+    #background.show
+    
+    #Apply a filter to the picture
+    if filterInput != '0':
+        if filterInput == '1':
+            image = im.filter(ImageFilter.BLUR)
+        elif filterInput == '2':
+            image = im.filter(ImageFilter.CONTOUR)
+        elif filterInput == '3':
+            image = im.filter(ImageFilter.DETAIL)
+        elif filterInput == '4':
+            image = im.filter(ImageFilter.EDGE_ENHANCE)
+        elif filterInput == '5':
+            image = im.filter(ImageFilter.EDGE_ENHANCE_MORE)
+        elif filterInput == '6':
+            image = im.filter(ImageFilter.EMBOSS)
+        elif filterInput == '7':
+            image = im.convert('L')
+        elif filterInput == '8':
+            converter = ImageEnhance.Color(image)
+            image = converter.enhance(3.0)
+        elif filterInput == '9':
+            image = im.filter(ImageFilter.SMOOTH_MORE)
+        elif filterInput == '10':
+            image = im.filter(ImageFilter.SHARPEN)
+        elif filterInput == '11':
+            image = im.convert('1')
+        
+        elif filterInput != 0:
+            print("You choose a invalid filter!")
+    else:
+        image = im
+        
+            #Apply border
+    if borderColor != "noBorder":
+        rgb = ImageColor.getrgb(borderColor)
+        img_w, img_h = image.size
+        im = Image.new('RGBA', (img_w+30, img_h+30), rgb)
+        bg_w, bg_h = im.size
+        offset = ((bg_w - img_w) / 2, (bg_h - img_h) / 2)
+        im.paste(image, offset)
+    else:
+        im = image
+        
+    im.save("static/img/newImage.png", "PNG")
+    
+    #result = {"Success": True}
 
     #Get image using PILLOW
-    pic = Image.open("static/img/img.png")
-    pic = pic.filter(ImageFilter.CONTOUR)
-    pic.save("static/img/newImage.png")
+    #pic = Image.open("static/img/img.png")
+    #pic = pic.filter(ImageFilter.CONTOUR)
+    #pic.save("static/img/newImage.png")
     
     with open("static/img/newImage.png", "rb") as imageFile:
         str = base64.b64encode(imageFile.read())
         result['image'] = str
-    
+
     #pic.show()
     
     return HttpResponse(json.dumps(result),content_type='application/json')
